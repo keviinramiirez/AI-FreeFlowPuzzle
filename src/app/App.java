d933a462cf9f46c63e32b334eaf12ca8e277d8f0
irixa <irixa.vales@upr.edu>
2019-05-04 19:56:08 -0400

    added class to create a puzzle

--- src/app/App.java
+++ src/app/App.java
@@ -15,6 +15,7 @@ import javax.swing.border.LineBorder;
 
 import interfaces.Entry;
 import util.HardCodedFlowPointers;
+import util.PuzzleCreator;
 
 import javax.swing.JComboBox;
 import javax.swing.JButton;
@@ -92,7 +93,10 @@ public class App {
 			}
 		}
 		grid.initializeEdges();
-
+		
+		PuzzleCreator pzl = new PuzzleCreator();
+		pzl.getPuzzle(grid);
+		
 		// Hard Coded Flow Pointers
 		HardCodedFlowPointers hardCodedPointers = new HardCodedFlowPointers(grid);
 		
@@ -114,7 +118,7 @@ public class App {
 //
 //		this.initGridFlowPointers(hardCodedPointers.initialPointers2_5x5());
 //		this.initGridFlowPointers(hardCodedPointers.initialPointers2_6x6());
-		this.initGridFlowPointers(hardCodedPointers.initialPointers2_7x7());
+		this.initGridFlowPointers(pzl.getPuzzle(grid));
 //		this.initGridFlowPointers(hardCodedPointers.initialPointers1_10x10());
 
 		// inserts the most constraint Initial Pointers within the priority queue
